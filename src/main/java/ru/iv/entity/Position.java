787f5cbff28dd40342d2a8d1dda3a60fe3aff4d9
vv <vasnikv@gmail.com>
2020-06-04 07:31:58 +0400

    справочники

---
+++ src/main/java/ru/iv/entity/Position.java
@@ -0,0 +1,42 @@
+package ru.iv.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Position {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    public String name;
+
+    @Column
+    private String fullName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
